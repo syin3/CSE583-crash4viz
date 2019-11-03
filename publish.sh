84cee5892fb6148168299123b011dfa0e3541def
Shuyi Yin <syin1@uw.edu>
2019-11-03 15:25:43 -0800

    Update

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ rm index.html
 # jupyter notebook --NotebookApp.iopub_data_rate_limit=1e10
 cp -i ../tech/$1.ipynb $1.ipynb
 cp -i ../tech/$2 $2
-jupyter-nbconvert --to slides $1 --reveal-prefix=reveal.js --SlidesExporter.reveal_scroll=True
+jupyter-nbconvert --to slides $1 --reveal-prefix=reveal.js
 
 mv $1.slides.html index.html
 mkdir -p /tmp/workspace
