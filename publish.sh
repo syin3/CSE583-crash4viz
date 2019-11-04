8a901a205e5dbff50e1434b8af3d788d2b9e3792
Shuyi Yin <syin1@uw.edu>
2019-11-04 08:51:49 -0800

    Update new

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ cp -i ../tech/$1.ipynb $1.ipynb
 cp -i ../tech/folium* .
 cp -i ../tech/bokeh* .
 cp -i ../tech/times.png .
-jupyter-nbconvert --to slides $1.ipynb --reveal-prefix=reveal.js --SlidesExporter.reveal_scroll=True
+jupyter-nbconvert --to slides $1.ipynb --reveal-prefix=reveal.js
 
 mv $1.slides.html index.html
 mkdir -p /tmp/workspace
