b28f9f7b0b83203d9fb6a6f8cde047990a7eb017
Katharine Chen <kychen37@uw.edu>
2019-11-28 00:53:38 -0800

    add mappig options for basic map and layered by year

--- mapping.py
+++ mapping.py
@@ -3,6 +3,7 @@ import pandas as pd
 import folium
 import folium.plugins
 import numpy as np
+import mapping_funcs
 
 class Maps:
     
@@ -10,6 +11,43 @@ class Maps:
     For example, they may choose to view clusters or layers, or all the data 
     marked directly on the map.
     (Note not all these choices have been coded into the interface.py yet)"""
+
+    def basic_map(self, grp_feature, subgrp_feature, incident_type, df, map_sink = 'MyMaps/test_filt.html'):
+
+        group_df = df.groupby(grp_feature)
+        subgrp_df = group_df.get_group(subgrp_feature)
+        incident_dict = mapping_funcs.r_incident_dict
+        
+        # create map object centered at the median location of the df
+        lat = df['Latitude'].median()
+        lon = df['Longitude'].median()
+        
+        accWA = folium.Map([lat, lon],
+                           prefer_canvas=True,
+                           zoom_start=8)
+    
+        for _, row in subgrp_df.iterrows():
+
+            if row[incident_type] > 0:
+                cirlColor = "#007849" 
+            
+            cirlRadius = row[incident_type] * 1.5
+            
+            folium.CircleMarker(
+                [row['Latitude'], row['Longitude']],
+                radius = cirlRadius,
+                weight = 0.2,
+                fill_color = cirlColor,
+                fill = True,
+                fill_opacity = 0.8,
+                popup = f'{incident_dict[incident_type]},{row[incident_type]}'
+            ).add_to(accWA)
+
+                                    
+        # save map
+        accWA.save(map_sink)
+    
+        return accWA
     
     
     def plot_folium_filtered_clusters(
@@ -47,17 +85,75 @@ class Maps:
             else:
                 cirlColor = 'steelblue'
             
-            cirlRadius = np.max(row[incident_type]) * 5
+            cirlRadius = row[incident_type] * 100
             
-            folium.CircleMarker([row['Latitude'], row['Longitude']],
-                                        radius=cirlRadius,
-                                        popup=folium.Popup("{}: {}".format(incident_type,
-                                        row[incident_type]), max_width=150),
-                                        weight = 0.2,
-                                        fill_color=cirlColor,
-                                        fill=True,
-                                        fill_opacity=0.4)
+            folium.CircleMarker(
+                [row['Latitude'], row['Longitude']],
+                radius=cirlRadius,
+                popup=folium.Popup("{}: {}".format(incident_type, row[incident_type]), max_width=150),
+                weight = 0.2,
+                fill_color=cirlColor,
+                fill=True,
+                fill_opacity=0.4)
                             
+        # save map
+        accWA.save(map_sink)
+    
+        return accWA
+
+    def plot_folium_filtered_layers(
+        self, grp_feature, subgrp_feature, incident_type, df, 
+        map_sink = 'MyMaps/test_filt.html'):
+        
+        group_df = df.groupby(grp_feature)
+        subgrp_df = group_df.get_group(subgrp_feature)
+        incident_dict = mapping_funcs.r_incident_dict
+        
+        # create map object centered at the median location of the df
+        lat = df['Latitude'].median()
+        lon = df['Longitude'].median()
+        
+        accWA = folium.Map([lat, lon],
+                        # tiles="cartodbpositron",
+                        tiles = '',
+                        # width='80%',
+                        # height='80%',
+                        prefer_canvas=True,
+                        zoom_start=8)
+
+        # add tile layer
+        folium.TileLayer('openstreetmap').add_to(accWA)
+        #folium.TileLayer('CartoDB dark_matter', name = 'dark').add_to(accWA)
+
+        start = 2008
+        end = 2017
+        layers = []
+
+        for year in range(start, end + 1):
+            layer = folium.FeatureGroup(name=str(year) + incident_type, show=False)
+            layers.append(layer)
+            accWA.add_child(layers[-1])
+
+            for _, row in subgrp_df[subgrp_df['Year'] == str(year)].iterrows():
+                
+                if row[incident_type] > 0:
+                    cirlColor = "#007849" 
+            
+                cirlRadius = row[incident_type] * 3
+            
+                folium.CircleMarker(
+                    [row['Latitude'], row['Longitude']],
+                    radius=cirlRadius,
+                    weight = 0.2,
+                    fill_color=cirlColor,
+                    fill=True,
+                    fill_opacity=0.8,
+                    popup = f'{incident_dict[incident_type]},{row[incident_type]}'
+                ).add_to(layers[-1])
+
+        # add layer control
+        folium.LayerControl().add_to(accWA)
+        
         # save map
         accWA.save(map_sink)
     
@@ -76,8 +172,10 @@ class Maps:
         for group, i in grouping:
             groups.append(group)
 
+        lat = df['Latitude'].median()
+        lon =  df['Longitude'].median()
         # create map object
-        accWA = folium.Map([df['Latitude'].median(), df['Longitude'].median()],
+        accWA = folium.Map([lat, lon],
                        # tiles="cartodbpositron",
                        tiles = '',
                        # width='80%',
