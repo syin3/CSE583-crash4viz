f471cf5f70838537d1e2a0e4ca018650b8892bac
Katharine Chen <kychen37@uw.edu>
2019-11-29 15:00:24 -0800

    suppress dtype warning

--- WAcrashviz/tests/test_mapping_funcs.py
+++ WAcrashviz/tests/test_mapping_funcs.py
@@ -7,8 +7,12 @@ parentdir = os.path.dirname(currentdir)
 parent = os.path.dirname(parentdir)
 sys.path.insert(0,parent)
 from WAcrashviz import mapping_funcs
+import warnings
 
 class TestMappingFuncs(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter('ignore')
 
     """Tests for ensuring elements of the data are loaded correctly. 
     Run these tests from the top-most directory of the WAcrashviz package."""
@@ -24,13 +28,13 @@ class TestMappingFuncs(unittest.TestCase):
     def test_paths(self):
 
         self.assertRegex(mapping_funcs.MODULE_DIR, 'WAcrashviz',
-                               "WAcrashviz module path incorrect")
+                               "Path to WAcrashviz module is incorrect")
         
         self.assertRegex(mapping_funcs.MAPS_DIR, '/MyMaps',
-                               "Maps not being saved in current directory")
+                               "Maps are not being saved in MyMaps directory")
 
         self.assertRegex(mapping_funcs.DATA_DIR, '/data/',
-                               "Path to HSIS data incorrect")
+                               "Path to HSIS crash data is incorrect")
 
 if __name__ == '__main__':
     unittest.main()
