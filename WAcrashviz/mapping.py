0fc47faec9c4c284617e0f12fbfc0f8e86c1396c
Katharine Chen <kychen37@uw.edu>
2019-12-01 13:25:08 -0800

    pylinting

--- WAcrashviz/mapping.py
+++ WAcrashviz/mapping.py
@@ -1,22 +1,26 @@
-import os
-import pandas as pd
+"""
+Functions for plotting the folium maps that users chose in the interface.
+For example, they may choose to view clusters or layers, or all the data 
+marked directly on the map.
+(Note not all these choices have been coded into the interface.py yet)
+"""
+
 import folium
 import folium.plugins
-import numpy as np
 from . import mapping_funcs
 import warnings
 warnings.filterwarnings('ignore')
 
 class Maps:
-    
-    """Functions for plotting the folium maps that users chose in the interface.
-    For example, they may choose to view clusters or layers, or all the data 
-    marked directly on the map.
-    (Note not all these choices have been coded into the interface.py yet)"""
 
-    def basic_map(self, grp_feature, subgrp_feature, incident_type, df, map_sink = None):
+    """Class encompassing all the functions for generating maps based on user
+    selections."""
 
-        group_df = df.groupby(grp_feature)
+    def basic_map(self, grp_feature, subgrp_feature, incident_type, data, map_sink = None):
+        """Creates a map with no layers or clusters, the chosent incident_type
+        is marked directly on the map at the lat/lon location that it occurs."""
+
+        group_df = data.groupby(grp_feature)
         subgrp_df = group_df.get_group(subgrp_feature)
         r_incident_dict = mapping_funcs.r_incident_dict
         incident = r_incident_dict[incident_type]
@@ -28,40 +32,42 @@ class Maps:
             map_sink =  map_dir + f'/{group}_{incident}_basic_map.html'
         
         # create map object centered at the median location of the df
-        lat = df['Latitude'].median()
-        lon = df['Longitude'].median()
+        lat = data['Latitude'].median()
+        lon = data['Longitude'].median()
         
-        accWA = folium.Map([lat, lon],
+        acc_wa = folium.Map([lat, lon],
                            prefer_canvas=True,
                            zoom_start=8)
     
         for _, row in subgrp_df.iterrows():
 
             if row[incident_type] > 0:
-                cirlColor = "#007849" 
+                cirlcolor = "#007849" 
             
-                cirlRadius = row[incident_type] * 1.5
+                cirlradius = row[incident_type] * 1.5
             
                 folium.CircleMarker(
                     [row['Latitude'], row['Longitude']],
-                    radius = cirlRadius,
+                    radius = cirlradius,
                     weight = 0.2,
-                    fill_color = cirlColor,
+                    fill_color = cirlcolor,
                     fill = True,
                     fill_opacity = 0.8,
                     popup = f'{incident},{row[incident_type]}'
-                ).add_to(accWA)
+                ).add_to(acc_wa)
                   
-        accWA.save(map_sink)
+        acc_wa.save(map_sink)
     
-        return accWA
+        return acc_wa
     
     
     def plot_folium_filtered_clusters(
-        self, grp_feature, subgrp_feature, incident_type, df, 
+        self, grp_feature, subgrp_feature, incident_type, data, 
         map_sink = None):
+        """Create a map with interactable clusters that describes the frequency
+        of the chosen incident_type by location."""
         
-        group_df = df.groupby(grp_feature)
+        group_df = data.groupby(grp_feature)
         subgrp_df = group_df.get_group(subgrp_feature)
         r_incident_dict = mapping_funcs.r_incident_dict
         incident = r_incident_dict[incident_type]
@@ -73,10 +79,10 @@ class Maps:
             map_sink =  map_dir + f'/{group}_{incident}_cluster_map.html'
         
         # create map object centered at the median location of the df
-        lat = df['Latitude'].median()
-        lon = df['Longitude'].median()
+        lat = data['Latitude'].median()
+        lon = data['Longitude'].median()
         
-        accWA = folium.Map([lat, lon],
+        acc_wa = folium.Map([lat, lon],
                         # tiles="cartodbpositron",
                         # tiles = '',
                         # width='80%',
@@ -88,29 +94,31 @@ class Maps:
         
         for _, row in subgrp_df.iterrows():
             if row[incident_type] > 0:
-                cirlColor = "#007849" 
-                cirlRadius = row[incident_type] * 10
+                cirlcolor = "#007849" 
+                cirlradius = row[incident_type] * 10
                 clust.add_child(folium.Marker(
                     location = [row['Latitude'], row['Longitude']],
-                    radius=cirlRadius,
+                    radius=cirlradius,
                     popup=folium.Popup("{}: {}".format(
                         incident, row[incident_type]), max_width=150),
                     weight = 0.2,
-                    fill_color=cirlColor,
+                    fill_color=cirlcolor,
                     fill=True,
                     fill_opacity=0.4))
 
-        accWA.add_child(clust)
+        acc_wa.add_child(clust)
 
-        accWA.save(map_sink)
+        acc_wa.save(map_sink)
     
-        return accWA
+        return acc_wa
 
     def plot_folium_filtered_layers(
-        self, grp_feature, subgrp_feature, incident_type, df, 
+        self, grp_feature, subgrp_feature, incident_type, data, 
         map_sink = None):
+        """Creates a map with layers showing the selected incident_type by
+        year."""
         
-        group_df = df.groupby(grp_feature)
+        group_df = data.groupby(grp_feature)
         subgrp_df = group_df.get_group(subgrp_feature)
         r_incident_dict = mapping_funcs.r_incident_dict
         incident = r_incident_dict[incident_type]
@@ -122,10 +130,10 @@ class Maps:
             map_sink =  map_dir + f'/{group}_{incident}_layer_map.html'
         
         # create map object centered at the median location of the df
-        lat = df['Latitude'].median()
-        lon = df['Longitude'].median()
+        lat = data['Latitude'].median()
+        lon = data['Longitude'].median()
         
-        accWA = folium.Map([lat, lon],
+        acc_wa = folium.Map([lat, lon],
                         # tiles="cartodbpositron",
                         tiles = '',
                         # width='80%',
@@ -134,8 +142,8 @@ class Maps:
                         zoom_start=8)
 
         # add tile layer
-        folium.TileLayer('openstreetmap').add_to(accWA)
-        #folium.TileLayer('CartoDB dark_matter', name = 'dark').add_to(accWA)
+        folium.TileLayer('openstreetmap').add_to(acc_wa)
+        #folium.TileLayer('CartoDB dark_matter', name = 'dark').add_to(acc_wa)
 
         start = 2008
         end = 2017
@@ -145,42 +153,42 @@ class Maps:
             layer = folium.FeatureGroup(
                 name=str(year) + ': ' + incident, show=False)
             layers.append(layer)
-            accWA.add_child(layers[-1])
+            acc_wa.add_child(layers[-1])
 
             for _, row in subgrp_df[subgrp_df['Year'] == str(year)].iterrows():
                 
                 if row[incident_type] > 0:
-                    cirlColor = "#007849" 
+                    cirlcolor = "#007849" 
             
-                    cirlRadius = row[incident_type] * 3
+                    cirlradius = row[incident_type] * 3
             
                     folium.CircleMarker(
                         [row['Latitude'], row['Longitude']],
-                        radius=cirlRadius,
+                        radius=cirlradius,
                         weight = 0.2,
-                        fill_color=cirlColor,
+                        fill_color=cirlcolor,
                         fill=True,
                         fill_opacity=0.8,
                         popup = f'{incident},{row[incident_type]}'
                     ).add_to(layers[-1])
 
         # add layer control
-        folium.LayerControl().add_to(accWA)
+        folium.LayerControl().add_to(acc_wa)
         
         # save map
-        map_dir = mapping_funcs.MAPS_DIR
-        mpsv =  map_dir + f'/{grp_feature}_{incident}_layer_map.html'
-        accWA.save(map_sink)
+        acc_wa.save(map_sink)
     
-        return accWA
+        return acc_wa
     
     
-    def plot_folium(self, feature, df, map_sink = None):
-        '''
-        @param df: dataframe wrangled for selected feature
+    def plot_folium(self, feature, data, map_sink = None):
+        '''Creates a very large map with layers, generates data for all 
+        possible groups. Will probably remove this map as it isn't really
+        useful for our usecases.
+        @param data: dataframe wrangled for selected feature
         @param map_sink: saving destination of generated map
         '''
-        grouping = df.groupby(feature)
+        grouping = data.groupby(feature)
         grp_dict = mapping_funcs.grp_dict
         
         if map_sink is None:
@@ -193,10 +201,10 @@ class Maps:
         for group, i in grouping:
             groups.append(group)
 
-        lat = df['Latitude'].median()
-        lon =  df['Longitude'].median()
+        lat = data['Latitude'].median()
+        lon =  data['Longitude'].median()
         # create map object
-        accWA = folium.Map([lat, lon],
+        acc_wa = folium.Map([lat, lon],
                        # tiles="cartodbpositron",
                        tiles = '',
                        # width='80%',
@@ -204,8 +212,8 @@ class Maps:
                        prefer_canvas=True,
                        zoom_start=8)
         # add tile layer
-        folium.TileLayer('cartodbpositron', name = 'bright').add_to(accWA)
-        folium.TileLayer('CartoDB dark_matter', name = 'dark').add_to(accWA)
+        folium.TileLayer('cartodbpositron', name = 'bright').add_to(acc_wa)
+        folium.TileLayer('CartoDB dark_matter', name = 'dark').add_to(acc_wa)
 
         # create crash layer
         crashes = []
@@ -213,38 +221,38 @@ class Maps:
         # individual crashes
         crash = folium.FeatureGroup(name=feature + '_Crashes', show=False)
         crashes.append(crash)
-        accWA.add_child(crashes[-1])
+        acc_wa.add_child(crashes[-1])
 
         for group in groups:
-            for _, row in df[df[feature] == group].iterrows():
+            for _, row in data[data[feature] == group].iterrows():
                 # group is the grouped weather condition
                 if row['# INJ'] > 0:
-                    cirlColor = "#007849" 
+                    cirlcolor = "#007849" 
                 elif row['# FAT'] > 0: 
-                    cirlColor = 'red'
+                    cirlcolor = 'red'
                 else:
-                    cirlColor = 'steelblue'
+                    cirlcolor = 'steelblue'
 
                 # define circle radius
                 if row['# INJ'] + row['# FAT'] > 0:
-                    cirlRadius = max(row['# INJ'], row['# FAT']) * 3
+                    cirlradius = max(row['# INJ'], row['# FAT']) * 3
                 else:
-                    cirlRadius = 1
+                    cirlradius = 1
 
                 folium.CircleMarker([row['Latitude'], row['Longitude']],
-                                    radius=cirlRadius,
+                                    radius=cirlradius,
                                     popup=folium.Popup("INJ: {}, FAT: {}".format(
                                     row['# INJ'], row['# FAT']), max_width=150),
                                     weight = 0.2,
-                                    fill_color=cirlColor,
+                                    fill_color=cirlcolor,
                                     fill=True,
                                     fill_opacity=0.4
                             ).add_to(crashes[-1])
 
         # add layer control
-        folium.LayerControl().add_to(accWA)
+        folium.LayerControl().add_to(acc_wa)
 
         # save map
-        accWA.save(map_sink)
+        acc_wa.save(map_sink)
 
-        return accWA
+        return acc_wa
