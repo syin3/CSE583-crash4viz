011da7e1ded2aa92e85c312ca32f96f9f2ff9861
Katharine Chen <kychen37@uw.edu>
2019-12-01 23:25:34 -0800

    move out of main repo

---
+++ misc/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm ./technology\ review/index.html
+# run the following in original folder
+# jupyter notebook --NotebookApp.iopub_data_rate_limit=1e10
+cp -i ../tech/$1.ipynb ./technology\ review/$1.ipynb
+cp -i ../tech/folium_*.html ./technology\ review/plots
+cp -i ../tech/bokeh_*.html ./technology\ review/plots
+cp -i ../tech/times.png ./technology\ review/plots
+jupyter nbconvert ./technology\ review/$1.ipynb --to slides
+#  --reveal-prefix=reveal.js
+
+mv ./technology\ review/$1.slides.html ./technology\ review/index.html
+mkdir -p /tmp/workspace
+cp -r * /tmp/workspace/
+git add -A .
+git commit -m "Update new"
+git checkout -B gh-pages
+cp -r /tmp/workspace/* .
+git add -A .
+git commit -m "new version"
+git push origin master gh-pages
+git checkout master
+rm -rf /tmp/workspace
