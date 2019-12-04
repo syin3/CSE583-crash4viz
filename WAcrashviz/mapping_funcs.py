748b08ebb3de41f7eaded8c341310f05b58faaca
Katharine Chen <kychen37@uw.edu>
2019-12-03 22:58:28 -0800

    update

--- WAcrashviz/mapping_funcs.py
+++ WAcrashviz/mapping_funcs.py
@@ -1,8 +1,11 @@
+"""
+Variables of the data that will be called from mapping.py to generate maps.
+Functions for cleaning and merging the different data sources and dataframes.
+"""
+import warnings
 import os
 import pandas as pd
-import folium
 import numpy as np
-import warnings
 warnings.filterwarnings('ignore')
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -13,78 +16,82 @@ DATA_DIR = MODULE_DIR + '/data/'
 """Common variables and functions that will be called by mapping.py"""
 
 # all possible variables, {label in dataframe: label in GUI}
-vars_dict = {}
-vars_dict['Weather'] = 'WEATHER'
-vars_dict['Surface Condition'] = 'ROADWAY SURFACE CONDITION'
-vars_dict['Lighting Condition'] = 'LIGHTING CONDITION'
-vars_dict['Junction Relationship'] = 'JUNCTION RELATIONSHIP'
-#vars_dict['TIME'] = 'Time'
+VARS_DICT = {}
+VARS_DICT['Weather'] = 'WEATHER'
+VARS_DICT['Surface Condition'] = 'ROADWAY SURFACE CONDITION'
+VARS_DICT['Lighting Condition'] = 'LIGHTING CONDITION'
+VARS_DICT['Junction Relationship'] = 'JUNCTION RELATIONSHIP'
+#VARS_DICT['TIME'] = 'Time'
 
 # reverse dict to above for writing output maps
-grp_dict = {}
-grp_dict['WEATHER'] = 'Weather'
-grp_dict['ROADWAY SURFACE CONDITION'] = 'Surface_Condition'
-grp_dict['LIGHTING CONDITION'] = 'Lighting_Condition'
-grp_dict['JUNCTION RELATIONSHIP'] = 'Junction_Relationship'
+GRP_DICT = {}
+GRP_DICT['WEATHER'] = 'Weather'
+GRP_DICT['ROADWAY SURFACE CONDITION'] = 'Surface_Condition'
+GRP_DICT['LIGHTING CONDITION'] = 'Lighting_Condition'
+GRP_DICT['JUNCTION RELATIONSHIP'] = 'Junction_Relationship'
 
-incident_dict = {}
-incident_dict['Injuries'] = '# INJ'
-incident_dict['Fatalities'] = '# FAT'
-incident_dict['Number of Vehicles Involved'] = '# VEH'
+INCIDENT_DICT = {}
+INCIDENT_DICT['Injuries'] = '# INJ'
+INCIDENT_DICT['Fatalities'] = '# FAT'
+INCIDENT_DICT['Number of Vehicles Involved'] = '# VEH'
 
-r_incident_dict = {}
-r_incident_dict['# INJ'] = 'Injuries'
-r_incident_dict['# FAT'] = 'Fatalities'
-r_incident_dict['# VEH'] = 'Number of Vehicles Involved'
+R_INCIDENT_DICT = {}
+R_INCIDENT_DICT['# INJ'] = 'Injuries'
+R_INCIDENT_DICT['# FAT'] = 'Fatalities'
+R_INCIDENT_DICT['# VEH'] = 'Number of Vehicles Involved'
 
-subgroups_dict = {}
-subgroups_dict['Weather'] = [
-    'Blowing Sand or Dirt or Snow', 
-    'Clear or Partly Cloudy', 
-    'Fog or Smog or Smoke', 
-    'Other', 
+SUBGROUPS_DICT = {}
+SUBGROUPS_DICT['Weather'] = [
+    'Blowing Sand or Dirt or Snow',
+    'Clear or Partly Cloudy',
+    'Fog or Smog or Smoke',
+    'Other',
     'Overcast',
-    'Raining', 
-    'Severe Crosswind', 
-    'Sleet or Hail or Freezing Rain', 
-    'Snowing', 
+    'Raining',
+    'Severe Crosswind',
+    'Sleet or Hail or Freezing Rain',
+    'Snowing',
     'Unknown']
-subgroups_dict['Surface Condition'] = [
-    'Dry', 
-    'Ice', 
-    'Oil', 
-    'Other', 
-    'Sand/Mud/Dirt', 
-    'Snow/Slush', 
-    'Standing Water', 
-    'Unknown', 
+SUBGROUPS_DICT['Surface Condition'] = [
+    'Dry',
+    'Ice',
+    'Oil',
+    'Other',
+    'Sand/Mud/Dirt',
+    'Snow/Slush',
+    'Standing Water',
+    'Unknown',
     'Wet']
-subgroups_dict['Lighting Condition'] = [
-    'Dark-No Street Lights', 
-    'Dark-Street Lights Off', 
-    'Dark-Street Lights On', 
-    'Dawn', 
-    'Daylight', 
-    'Dusk', 
-    'Other', 
+SUBGROUPS_DICT['Lighting Condition'] = [
+    'Dark-No Street Lights',
+    'Dark-Street Lights Off',
+    'Dark-Street Lights On',
+    'Dawn',
+    'Daylight',
+    'Dusk',
+    'Other',
     'Unknown']
-subgroups_dict['Junction Relationship'] = [
-    'At Driveway', 
-    'At Driveway within Major Intersection', 
-    'At Intersection and Not Related', 
-    'At Intersection and Related', 
-    'At Roundabout but not Related', 
-    'Circulating Roundabout', 
+SUBGROUPS_DICT['Junction Relationship'] = [
+    'At Driveway',
+    'At Driveway within Major Intersection',
+    'At Intersection and Not Related',
+    'At Intersection and Related',
+    'At Roundabout but not Related',
+    'Circulating Roundabout',
     'Driveway Related but Not at Driveway',
     'Entering Roundabout',
     'Exiting Roundabout',
     'Intersection Related but Not at Intersection',
     'Not at Intersection and Not Related',
     'Roundabout Related but not at Roundabout']
-# subgroups_dict['TIME'] = [] could add something about night/day -- although also covered in lighting
+# SUBGROUPS_DICT['TIME'] = [] could add something about time
 
 
 def clean_dataframe():
+    """Cleaning the data used in in the app for pertinent information so it can
+    be called on efficiently when generating the map based on user inputs at
+    the interface."""
+
     coords = pd.read_csv(DATA_DIR + 'coords_gps.csv')
     crashes = pd.read_csv(DATA_DIR + 'WA_Rural_St_RtesCrashes_Full.csv')
     # change the coordinates to be regular lat/lon
@@ -92,9 +99,19 @@ def clean_dataframe():
     crashes['Latitude'] = np.array(coords['Latitude'])
     crashes['Longitude'] = np.array(coords['Longitude'])
     # filter for general columns of interest
-    crash_df = crashes.filter(["COUNTY", "DATE", "TIME", "MOST SEVERE INJURY TYPE", "WEATHER", "ROADWAY SURFACE CONDITION", "LIGHTING CONDITION", "JUNCTION RELATIONSHIP", "# INJ", "# FAT", "# VEH", "# PEDS", "# BIKES", "Latitude", "Longitude"])
+    crash_df = crashes.filter(["COUNTY",
+                               "DATE",
+                               "TIME",
+                               "MOST SEVERE INJURY TYPE",
+                               "WEATHER",
+                               "ROADWAY SURFACE CONDITION",
+                               "LIGHTING CONDITION",
+                               "JUNCTION RELATIONSHIP",
+                               "# INJ", "# FAT", "# VEH",
+                               "# PEDS", "# BIKES",
+                               "Latitude", "Longitude"])
     years = []
     for date in crash_df['DATE']:
-        years.append(date.split('/')[2])    
+        years.append(date.split('/')[2])
     crash_df['Year'] = years
     return crash_df
--- WAcrashviz/tests/test_mapping.py
+++ WAcrashviz/tests/test_mapping.py
@@ -1,30 +1,40 @@
+"""Tests for generating the different map options available to users.
+The output is the example map as well as a tsv file containing the time
+it took to generate the map."""
+
 import unittest
 import os
-import inspect
-import sys
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-parent = os.path.dirname(parentdir)
-sys.path.insert(0,parent)
-from WAcrashviz import mapping
-from WAcrashviz import mapping_funcs
 import warnings
 import time
+import inspect
+import sys
+CURRENTDIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+PARENTDIR = os.path.dirname(CURRENTDIR)
+PARENT = os.path.dirname(PARENTDIR)
+sys.path.insert(0, PARENT)
+from wacrashviz import mapping
+from wacrashviz import mapping_funcs
+
+TEST_OUTPUT = 'test_output/generation_times.tsv'
 
 class TestMapping(unittest.TestCase):
+    """Test the different map options we have, the inputs to these mapping
+    functions are set within each function, so will differ slightly for
+    different features.
+    """
 
     def setUp(self):
         warnings.simplefilter('ignore')
 
     def test_basic_map(self):
 
-        """Use this test to assess how much time a basic map takes to 
+        """Use this test to assess how much time a basic map takes to
         generate. However, this will vary depending on how much data belongs
-         to the test_grp_feature and test_subgrp_feature. Run from the 
+         to the test_grp_feature and test_subgrp_feature. Run from the
          top-most level of the WAcrashviz package."""
-        
-        r_incident_dict = mapping_funcs.r_incident_dict
-        grp_dict = mapping_funcs.grp_dict
+
+        r_incident_dict = mapping_funcs.R_INCIDENT_DICT
+        grp_dict = mapping_funcs.GRP_DICT
         test_grp_feature = 'WEATHER'
         test_grp = grp_dict[test_grp_feature]
         test_subgrp_feature = 'Raining'
@@ -32,31 +42,33 @@ class TestMapping(unittest.TestCase):
         test_inc = r_incident_dict[test_incident_type]
         test_df = mapping_funcs.clean_dataframe()
         testmap = mapping.Maps()
-        map_sink = currentdir + '/test_output/'
+        map_sink = CURRENTDIR + '/test_output/'
         map_sink = map_sink + f'{test_grp}_{test_inc}_basic_map_test.html'
-        t0 = time.time()
+        t_zero = time.time()
         testmap.basic_map(
             test_grp_feature,
             test_subgrp_feature,
             test_incident_type,
             test_df,
             map_sink)
-        t1 = time.time()
-        
-        total = t1- t0
+        t_one = time.time()
+
+        total = t_one - t_zero
         time_to_generate = 'basic map generation time is ' + str(total)
-            
-        print(time_to_generate)
-        
+
+
+        with open(TEST_OUTPUT, 'a') as output:
+            output.write(time_to_generate)
+
     def test_cluster_map(self):
 
-        """Use this test to assess how much time a cluster map takes to 
+        """Use this test to assess how much time a cluster map takes to
         generate. However, this will vary depending on how much data belongs
-         to the test_grp_feature and test_subgrp_feature. Run from the 
+         to the test_grp_feature and test_subgrp_feature. Run from the
          top-most level of the WAcrashviz package."""
-        
-        r_incident_dict = mapping_funcs.r_incident_dict
-        grp_dict = mapping_funcs.grp_dict
+
+        r_incident_dict = mapping_funcs.R_INCIDENT_DICT
+        grp_dict = mapping_funcs.GRP_DICT
         test_grp_feature = 'WEATHER'
         test_grp = grp_dict[test_grp_feature]
         test_subgrp_feature = 'Raining'
@@ -64,31 +76,32 @@ class TestMapping(unittest.TestCase):
         test_inc = r_incident_dict[test_incident_type]
         test_df = mapping_funcs.clean_dataframe()
         testmap = mapping.Maps()
-        map_sink = currentdir + '/test_output/'
+        map_sink = CURRENTDIR + '/test_output/'
         map_sink = map_sink + f'{test_grp}_{test_inc}_cluster_map_test.html'
-        t0 = time.time()
+        t_zero = time.time()
         testmap.plot_folium_filtered_clusters(
             test_grp_feature,
             test_subgrp_feature,
             test_incident_type,
             test_df,
             map_sink)
-        t1 = time.time()
-        
-        total = t1- t0
+        t_one = time.time()
+
+        total = t_one - t_zero
         time_to_generate = 'cluster map generation time is ' + str(total)
-            
-        print(time_to_generate)
-        
+
+        with open(TEST_OUTPUT, 'a') as output:
+            output.write(time_to_generate)
+
     def test_layer_map(self):
 
-        """Use this test to assess how much time a layer map takes to 
+        """Use this test to assess how much time a layer map takes to
         generate. However, this will vary depending on how much data belongs
-         to the test_grp_feature and test_subgrp_feature. Run from the 
+         to the test_grp_feature and test_subgrp_feature. Run from the
          top-most level of the WAcrashviz package."""
-        
-        r_incident_dict = mapping_funcs.r_incident_dict
-        grp_dict = mapping_funcs.grp_dict
+
+        r_incident_dict = mapping_funcs.R_INCIDENT_DICT
+        grp_dict = mapping_funcs.GRP_DICT
         test_grp_feature = 'WEATHER'
         test_grp = grp_dict[test_grp_feature]
         test_subgrp_feature = 'Raining'
@@ -96,49 +109,50 @@ class TestMapping(unittest.TestCase):
         test_inc = r_incident_dict[test_incident_type]
         test_df = mapping_funcs.clean_dataframe()
         testmap = mapping.Maps()
-        map_sink = currentdir + '/test_output/'
+        map_sink = CURRENTDIR + '/test_output/'
         map_sink = map_sink + f'{test_grp}_{test_inc}_layer_map_test.html'
-        t0 = time.time()
+        t_zero = time.time()
         testmap.plot_folium_filtered_layers(
             test_grp_feature,
             test_subgrp_feature,
             test_incident_type,
             test_df,
             map_sink)
-        t1 = time.time()
-        
-        total = t1- t0
+        t_one = time.time()
+
+        total = t_one - t_zero
         time_to_generate = 'layer map generation time is ' + str(total)
-            
-        print(time_to_generate)
-        
-        
+
+        with open(TEST_OUTPUT, 'a') as output:
+            output.write(time_to_generate)
+
     def test_gigantic_map(self):
 
-        """Use this test to assess how much time the big map takes to 
+        """Use this test to assess how much time the big map takes to
         generate. However, this will vary depending on how much data belongs
-         to the test_grp_feature and test_subgrp_feature. Run from the 
+         to the test_grp_feature and test_subgrp_feature. Run from the
          top-most level of the WAcrashviz package."""
-        
-        r_incident_dict = mapping_funcs.r_incident_dict
-        grp_dict = mapping_funcs.grp_dict
+
+        r_incident_dict = mapping_funcs.R_INCIDENT_DICT
+        grp_dict = mapping_funcs.GRP_DICT
         test_grp_feature = 'WEATHER'
         test_grp = grp_dict[test_grp_feature]
         test_df = mapping_funcs.clean_dataframe()
         testmap = mapping.Maps()
-        map_sink = currentdir + '/test_output/'
+        map_sink = CURRENTDIR + '/test_output/'
         map_sink = map_sink + f'{test_grp}_big_map_test.html'
-        t0 = time.time()
+        t_zero = time.time()
         testmap.plot_folium(
             test_grp_feature,
             test_df,
             map_sink)
-        t1 = time.time()
-        
-        total = t1- t0
+        t_one = time.time()
+
+        total = t_one - t_zero
         time_to_generate = 'gigantic map generation time is ' + str(total)
-            
-        print(time_to_generate)
+
+        with open(TEST_OUTPUT, 'a') as output:
+            output.write(time_to_generate)
 
 if __name__ == '__main__':
     unittest.main()
